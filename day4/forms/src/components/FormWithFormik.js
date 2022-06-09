15c5d627e381bfe003362c12feed5f53d75940cd
Mehmet Seven <mehmetseven0@gmail.com>
2022-06-09 11:03:20 +0300

    FormikForm with useFormik hook

--- day4/forms/src/components/FormWithFormik.js
+++ day4/forms/src/components/FormWithFormik.js
@@ -1,81 +1,78 @@
 import React from "react";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import validationSchema from "./validations";
 
 function FormWithFormik() {
+  const {
+    handleSubmit,
+    values,
+    handleBlur,
+    handleChange,
+    touched,
+    errors,
+    isSubmitting,
+  } = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+    onSubmit: async (values, bag) => {
+      await new Promise((r) => setTimeout(r, 1000));
+      console.log(values);
+      bag.resetForm();
+    },
+    validationSchema,
+  });
+
   return (
     <div className="contact-form">
-      <Formik
-        initialValues={{
-          name: "",
-          email: "",
-          message: "",
-        }}
-        onSubmit={async (values, bag) => {
-          await new Promise((r) => setTimeout(r, 1000));
-
-          console.log(values);
-          bag.resetForm();
-        }}
-        validationSchema={validationSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="name">Name</label>
-              <input
-                name="name"
-                value={values.name}
-                onChange={handleChange("name")}
-                onBlur={handleBlur("name")}
-                disabled={isSubmitting}
-              />
-              {errors.name && touched.name && (
-                <div className="error">{errors.name}</div>
-              )}
-            </div>
-            <div>
-              <label htmlFor="email">E-mail</label>
-              <input
-                name="email"
-                value={values.email}
-                onChange={handleChange("email")}
-                onBlur={handleBlur("email")}
-                disabled={isSubmitting}
-              />
-              {errors.email && touched.email && (
-                <div className="error">{errors.email}</div>
-              )}
-            </div>
-            <div>
-              <label htmlFor="message">Message</label>
-              <textarea
-                name="message"
-                value={values.message}
-                onChange={handleChange("message")}
-                onBlur={handleBlur("message")}
-                disabled={isSubmitting}
-              />
-              {errors.message && touched.message && (
-                <div className="error">{errors.message}</div>
-              )}
-            </div>
-            <div>
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </div>
-          </form>
-        )}
-      </Formik>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            name="name"
+            value={values.name}
+            onChange={handleChange("name")}
+            onBlur={handleBlur("name")}
+            disabled={isSubmitting}
+          />
+          {errors.name && touched.name && (
+            <div className="error">{errors.name}</div>
+          )}
+        </div>
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input
+            name="email"
+            value={values.email}
+            onChange={handleChange("email")}
+            onBlur={handleBlur("email")}
+            disabled={isSubmitting}
+          />
+          {errors.email && touched.email && (
+            <div className="error">{errors.email}</div>
+          )}
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            value={values.message}
+            onChange={handleChange("message")}
+            onBlur={handleBlur("message")}
+            disabled={isSubmitting}
+          />
+          {errors.message && touched.message && (
+            <div className="error">{errors.message}</div>
+          )}
+        </div>
+        <div>
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
